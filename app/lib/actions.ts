38ad2f37a626ab21fe83d9f5561bbf0059444f72
Mahjabeen Akter <ext-mahjabeen.akter@elisa.fi>
2024-01-12 12:22:11 +0200

    edit invoice using server action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,15 +5,17 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+
 const FormSchema = z.object({
     id: z.string(),
     customerId: z.string(),
     amount: z.coerce.number(),
     status: z.enum(['pending', 'paid']),
     date: z.string(),
-  });
+  });// the amount field is specifically set to coerce(change) from a string to number while also validating its type
 
-  // the amount field is specifically set to coerce(change) from a string to number while also validating its type
+  // Use Zod to update the expected types
+  const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
   const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
@@ -36,3 +38,22 @@ export async function createInvoice(formData:FormData) {
     revalidatePath('/dashboard/invoices'); /*Once the database has been updated, the /dashboard/invoices path will be revalidated, and fresh data will be fetched from the server.*/
     redirect('/dashboard/invoices');
 }
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+   
+    const amountInCents = amount * 100;
+   
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+   
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+  }
